2c59a5bb8a4971b30f26a891b221a716d926c67e
黃敏雄 <48427735+Huang-Min-Xiong@users.noreply.github.com>
2021-06-12 01:10:05 +0800

    Update Download_Videos_from_YouTube.py

--- Download_Videos_from_YouTube.py
+++ Download_Videos_from_YouTube.py
@@ -1,4 +1,3 @@
-import pytube
 from pytube import YouTube
 
 #進度條
@@ -6,11 +5,11 @@ def progress(chunk, file_handle, bytes_remaining):
     contentSize = video.filesize
     size = contentSize - bytes_remaining
 
-    print('\r' + '[Download progress]:[%s%s]%.2f%%;' % 
+    print('\r' + '[Download progress]:[%s%s]%.2f%%\n' % 
     ('█' * int(size*20/contentSize), ' '*(20-int(size*20/contentSize)), float(size/contentSize*100)), end='')
+    print('下載完畢!\n')
 
-Youtube_url =input('請輸入網址:')
+Youtube_url =input('請輸入網址:') 
 yt = YouTube(Youtube_url, on_progress_callback=progress)
 video = yt.streams.first()
 video.download()
-print('下載完畢!\n')
